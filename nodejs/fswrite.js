fccc2f0884b4dc95a0da7d3157ee4ff19a4ee95b
MANOGNA REDDY MALLANNAGARI <2210030016@klh.edu.in>
2025-03-05 23:53:43 +0530

    Add files via upload

--- nodejs/fswrite.js
+++ nodejs/fswrite.js
@@ -1,12 +1,9 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const data = "Hello, Node.js!, This is added Content.";
-fs.writeFile("examplewrite.txt", data, (err) => {
-  if (err) {
-    console.error("Error writing file:", err);
-    return;
-  }
-  console.log(
-    "File written successfully!" + "The data writed into file-> " + data
-  );
+fs.writeFile('examplewrite.txt', 'Hello, Node.js!, This is added Content.', (err) => {
+    if (err) {
+        console.error('Error writing file:', err);
+        return;
+    }
+    console.log('File written successfully!');
 });
